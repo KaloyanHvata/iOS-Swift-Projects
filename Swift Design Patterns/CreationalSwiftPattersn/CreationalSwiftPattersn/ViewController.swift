d4876f6109b50d9e9ce69d109278328215df9d05
Kaloyan Petrov <kpetrov@itlite.onmicrosoft.com>
2015-11-06 14:28:16 +0200

    Firs Section in Design Patterns

---
+++ Swift Design Patterns/CreationalSwiftPattersn/CreationalSwiftPattersn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreationalSwiftPattersn
+//
+//  Created by Kaloyan Petrov on 11/6/15.
+//  Copyright © 2015 Kaloyan Petrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
