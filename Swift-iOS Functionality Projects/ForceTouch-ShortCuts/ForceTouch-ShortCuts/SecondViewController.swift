3a55d6e199c27d9c9aacc2dea5ba0cb888d3c283
Kaloyan Petrov <kpetrov@itlite.onmicrosoft.com>
2015-12-12 12:12:18 +0200

    Force Touch Example (Dynamic and Static Icons)

---
+++ Swift-iOS Functionality Projects/ForceTouch-ShortCuts/ForceTouch-ShortCuts/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ForceTouch-ShortCuts
+//
+//  Created by Kaloyan Petrov on 12/12/15.
+//  Copyright © 2015 kaloyanpetrov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
